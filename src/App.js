fabc4acaa9841a7c737a6e9758c64e0e92afbf4a
mohazahid <adyanzahid786@gmail.com>
2022-12-18 23:53:52 -0800

    react page new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'
+import Home from './pages/home'
+import AboutMe from './pages/aboutme'
+import './styles.css'
+// import Game from './pages/game'
+// import Credits from './pages/credits'
+// import Logs from './pages/logs'
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
-  );
+  <>
+    <Navbar />
+      <div className="containers">
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path ="/aboutme" element={<AboutMe />} />
+          {/* <Route path ="/" element={<Credits />} />
+          <Route path ="/" element={<Logs />} />  */}
+        </Routes>
+      </div>
+  </>
 }
 
 export default App;
