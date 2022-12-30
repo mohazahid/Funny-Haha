33d813d67a89b6bf55d97eed46764f8197f40e2d
mohazahid <adyanzahid786@gmail.com>
2022-12-30 02:34:45 -0800

    grid working

---
+++ src/pages/Images.js
@@ -0,0 +1,13 @@
+const Tatos = {
+    src: "MovieLogoTatos.png",
+    tags: ["C++", "Backend", "Java"],
+    title: "Tatos",
+    description: "This is a description of the project",
+    alt: "Tatos",
+    id: "Tatos",
+} 
+const imgArr = [
+    Tatos, 
+   
+];
+export default imgArr;
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,9 +1,33 @@
+import React from "react";
+import imgArr from "./Images";
+
+
+
 export default function Work() {
   return (
     <div className = "WorkPage ">
       <h1> Projects</h1>
-        <div className = "WorkPage_grid">
-            <div className = "WorkPage_grid_item1"> </div>
+      <div className = "WorkPage_grid_item">
+
+         {imgArr.map((imgsr) =>
+          //<img src={imgsr.src} className = "gridPhoto" alt="passedImg"/>
+          <CustomGrid key = {imgsr.id} imgsrc = {imgsr.src}  title = "Project 1" description = "This is a description of the project" link = "https://www.google.com"/>
+        )}
+      </div>
+      
+      
+      </div>
+    
+  );
+}
+function CustomGrid(imgsrc, title, description, link) {
+  console.log("images/ProjectsImages/"+imgsrc.imgsrc);
+  return (
+      <div className = "WorkPage_grid">
+        <div className = "Q1_area"> 
+          <img src= {"images/ProjectsImages/"+imgsrc.imgsrc} className = "gridPhoto" alt="passedImg"/>
+        </div>
+           
             <div className = "WorkPage_grid_item2">
               <h2 className = "h2 item2">
                 Skills
@@ -20,6 +44,4 @@ export default function Work() {
             <div className = "WorkPage_grid_item3"> 3</div>
             <div className = "WorkPage_grid_item4"> 4</div>
       </div>
-    </div>
-  );
-}
+    )};
